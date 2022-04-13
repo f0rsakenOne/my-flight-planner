8bd4a2fd207f9205e352a86251d3cbf182e2d46f
f0rsakenOne <nikolaj.aleksejenkov@gmail.com>
2022-04-13 20:29:12 +0300

    first commit

---
+++ src/main/java/com/example/myflightplanner/MyFlightPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.myflightplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFlightPlannerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MyFlightPlannerApplication.class, args);
+  }
+
+}
