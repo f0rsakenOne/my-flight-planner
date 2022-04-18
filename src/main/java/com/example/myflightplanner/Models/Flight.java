b618ebc03d83a2a3b4239e24eefd5c07637fb079
f0rsakenOne <nikolaj.aleksejenkov@gmail.com>
2022-04-19 00:52:28 +0300

    passed 01-testing.test.ts + some models

---
+++ src/main/java/com/example/myflightplanner/Models/Flight.java
@@ -0,0 +1,12 @@
+package com.example.myflightplanner.Models;
+
+public class Flight {
+  private int id;
+  private Airport from;
+  private Airport to;
+  private String carrier;
+  private String departureTime;
+  private String arrivalTime;
+
+
+}
